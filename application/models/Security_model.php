452abca82c85a34ad7e603f53140a3f8502940d0
Aldhisaputra <aldhisaputra795@gmail.com>
2024-06-26 23:11:43 +0700

    new

--- application/models/Security_model.php
+++ application/models/Security_model.php
@@ -2,12 +2,5 @@
 
 class Security_model extends CI_model{
 
-  public function getSecurity(){
-    $username = $this->session->userdata('username');
-
-    if(empty($username)){
-      $this->session->sess_destroy();
-      redirect('login');
-    }
-  }
+  
 }
